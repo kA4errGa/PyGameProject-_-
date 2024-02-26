a2da9d29d2fd33ce900bd68324ba749f8129e537
Artyom <wantplaying@yandex.ru>
2024-02-26 15:43:29 +0300

    чистка кода

--- LegendPole.py
+++ LegendPole.py
@@ -8,33 +8,33 @@ class LegendPole:
 
     def paint_legend(self, s):
         for i in range(4):
-            self.y_cord = 390 + 49 * i
+            y_cord = 390 + 49 * i
             if self.names[i] == 'Сумрачный лес':
-                self.mainfont = font.Font('Merriweather/Merriweather-Regular.ttf', 30)
-                self.text = (self.mainfont).render('Сумрачный лес', True, 'green')
-                s.blit(self.text, (714, self.y_cord))
+                mainfont = font.Font('Merriweather/Merriweather-Regular.ttf', 30)
+                text = mainfont.render('Сумрачный лес', True, 'green')
+                s.blit(text, (714, y_cord))
             if self.names[i] == 'Сокровищница дракона':
-                self.mainfont = font.Font('Merriweather/Merriweather-Regular.ttf', 20)
-                self.text = (self.mainfont).render('Сокровищница дракона', True, 'red')
-                s.blit(self.text, (714, self.y_cord))
+                mainfont = font.Font('Merriweather/Merriweather-Regular.ttf', 20)
+                text = mainfont.render('Сокровищница дракона', True, 'red')
+                s.blit(text, (714, y_cord))
             if self.names[i] == 'Большой город':
-                self.mainfont = font.Font('Merriweather/Merriweather-Regular.ttf', 30)
-                self.text = (self.mainfont).render('Большой город', True, 'blue')
-                s.blit(self.text, (714, self.y_cord))
+                mainfont = font.Font('Merriweather/Merriweather-Regular.ttf', 30)
+                text = mainfont.render('Большой город', True, 'blue')
+                s.blit(text, (714, y_cord))
             if self.names[i] == 'Логово чародея':
-                self.mainfont = font.Font('Merriweather/Merriweather-Regular.ttf', 30)
-                self.text = (self.mainfont).render('Логово чародея', True, 'purple')
-                s.blit(self.text, (714, self.y_cord))
+                mainfont = font.Font('Merriweather/Merriweather-Regular.ttf', 30)
+                text = mainfont.render('Логово чародея', True, 'purple')
+                s.blit(text, (714, y_cord))
             if self.names[i] == 'Деревня':
-                self.mainfont = font.Font('Merriweather/Merriweather-Regular.ttf', 30)
-                self.text = (self.mainfont).render('Деревня', True, 'orange')
-                s.blit(self.text, (714, self.y_cord))
+                mainfont = font.Font('Merriweather/Merriweather-Regular.ttf', 30)
+                text = mainfont.render('Деревня', True, 'orange')
+                s.blit(text, (714, y_cord))
             if self.names[i] == 'Хлебные поля':
-                self.mainfont = font.Font('Merriweather/Merriweather-Regular.ttf', 30)
-                self.text = (self.mainfont).render('Хлебные поля', True, 'yellow')
-                s.blit(self.text, (714, self.y_cord))
+                mainfont = font.Font('Merriweather/Merriweather-Regular.ttf', 30)
+                text = mainfont.render('Хлебные поля', True, 'yellow')
+                s.blit(text, (714, y_cord))
             if self.names[i] == 'Эльфийское озеро':
-                self.mainfont = font.Font('Merriweather/Merriweather-Regular.ttf', 26)
-                self.text = (self.mainfont).render('Эльфийское озеро', True, 'cyan')
-                s.blit(self.text, (714, self.y_cord))
+                mainfont = font.Font('Merriweather/Merriweather-Regular.ttf', 26)
+                text = mainfont.render('Эльфийское озеро', True, 'cyan')
+                s.blit(text, (714, y_cord))
             self.count += 1
