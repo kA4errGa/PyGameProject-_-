4bc5184a8ffff85fd4df3932a3acba6ee1737f18
Artyom <wantplaying@yandex.ru>
2024-02-26 15:45:08 +0300

    чистка кода

--- Score.py
+++ Score.py
@@ -7,12 +7,12 @@ class Score:
         self.score = 0
 
     def paint_score(self, s):
-        self.scorenamefont = font.Font('Merriweather/Merriweather-Regular.ttf', 50)
-        self.textscorename = (self.scorenamefont).render('Счёт', True, (255, 255, 255))
+        scorenamefont = font.Font('Merriweather/Merriweather-Regular.ttf', 50)
+        self.textscorename = scorenamefont.render('Счёт', True, (255, 255, 255))
         s.blit(self.textscorename, (34, 404))
-        self.scorefont = font.Font('Merriweather/Merriweather-Regular.ttf', 50)
-        self.textscore = (self.scorefont).render(str(self.score), True, (255, 255, 255))
-        s.blit(self.textscore, (34, 504))
+        scorefont = font.Font('Merriweather/Merriweather-Regular.ttf', 50)
+        textscore = scorefont.render(str(self.score), True, (255, 255, 255))
+        s.blit(textscore, (34, 504))
 
     def score_up(self, a):
         self.score += a
